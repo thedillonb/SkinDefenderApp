6c84208da27588f927bfd0d5004f5d16b5caff42
Dillon Buchanan <thedillonb@gmail.com>
2014-07-08 20:59:15 -0400

    Added Articles

---
+++ SkinSafeApp/TableViewCells/ArticleTableViewCell.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace SkinSafeApp.TableViewCells
+{
+    public partial class ArticleTableViewCell : UITableViewCell
+    {
+        public static readonly UINib Nib = UINib.FromName("ArticleTableViewCell", NSBundle.MainBundle);
+        public static readonly NSString Key = new NSString("ArticleTableViewCell");
+
+        public ArticleTableViewCell(IntPtr handle) : base(handle)
+        {
+        }
+
+        public UIImage Image
+        {
+            get { return ImageView.Image; }
+            set { ImageView.Image = value; }
+        }
+
+        public string Title
+        {
+            get { return TitleLabel.Text; }
+            set { TitleLabel.Text = value; }
+        }
+
+        public string Description
+        {
+            get { return DescriptionLabel.Text; }
+            set { DescriptionLabel.Text = value; }
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            this.TitleLabel.PreferredMaxLayoutWidth = this.TitleLabel.Frame.Width;
+            this.DescriptionLabel.PreferredMaxLayoutWidth = this.DescriptionLabel.Frame.Width;
+
+            this.ContentView.SetNeedsLayout();
+            this.ContentView.LayoutIfNeeded();
+        }
+
+        public static ArticleTableViewCell Create()
+        {
+            var cell = (ArticleTableViewCell)Nib.Instantiate(null, null)[0];
+            cell.ImageView.Layer.MasksToBounds = true;
+            cell.ImageView.Layer.CornerRadius = 6f;
+            cell.SeparatorInset = new UIEdgeInsets(0, 88f, 0, 0);
+            return cell;
+        }
+    }
+}
+
